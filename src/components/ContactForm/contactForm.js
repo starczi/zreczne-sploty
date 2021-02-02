b69da09976ba9c9f5879efbb6805b99e118ae210
starczi <jak.starczewski@gmail.com>
2021-02-02 18:06:05 +0100

    rm recaptcha

--- src/components/ContactForm/contactForm.js
+++ src/components/ContactForm/contactForm.js
@@ -84,7 +84,6 @@ export default function ContactForm() {
       method="post"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
-      data-netlify-recaptcha="true"
     >
       <input type="hidden" name="form-name" value="contact" />
       <label htmlFor="name">Name</label>
@@ -114,7 +113,6 @@ export default function ContactForm() {
         value={formState.message}
         placeholder="Enter your message"
       />
-      <div data-netlify-recaptcha="true"></div>
       <button type="submit">Submit</button>
     </StyledForm>
   );
