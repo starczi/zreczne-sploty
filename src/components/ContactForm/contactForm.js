0a237d1540f7239aeab511ae6c81dd7cfb408367
starczi <jak.starczewski@gmail.com>
2021-02-02 22:31:59 +0100

    Add finalish form of form v2

--- src/components/ContactForm/contactForm.js
+++ src/components/ContactForm/contactForm.js
@@ -1,11 +1,10 @@
 import React from 'react';
-// import { navigate } from 'gatsby-link';
 import styled from 'styled-components';
 
 const StyledForm = styled.form`
   max-width: 500px;
   margin: 2rem auto;
-  border: 2px solid #e74c3c;
+  border: 2px solid ${({ theme }) => theme.colors.black};
   padding: 2rem;
   border-radius: 5px;
 
@@ -20,69 +19,27 @@ const StyledForm = styled.form`
   textarea {
     display: block;
     width: 100%;
-    border: 2px solid #2980b9;
+    border: 2px solid ${({ theme }) => theme.colors.black};
     padding: 0.5rem;
-    font-size: 18px;
     border-radius: 5px;
   }
 
   button {
-    border: 0;
-    background: #343050;
+    background: ${({ theme }) => theme.colors.black};
     padding: 0.5rem;
     color: white;
+    border-radius: 5px;
     margin: 1rem 0;
     width: auto;
     text-transform: uppercase;
-    cursor: pointer;
     transition: 0.3s background ease;
     &:hover {
-      background: #3498db;
-    }
-
-    div {
-      width: 100%;
+      background: ${({ theme }) => theme.colors.lightGrey};
     }
   }
 `;
 
-// const encode = (data) => {
-//   Object.keys(data)
-//     .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-//     .join('&');
-// };
-
 export default function ContactForm() {
-  // const [formState, setFormState] = useState({
-  //   name: '',
-  //   email: '',
-  //   message: '',
-  // });
-
-  // const handleChange = (e) => {
-  //   setFormState({
-  //     ...formState,
-  //     [e.target.name]: e.target.value,
-  //   });
-  // };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   const form = e.target;
-  //   // eslint-disable-next-line no-undef
-  //   fetch('/contact/?no-cache=1', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //     body: encode({
-  //       'form-name': form.getAttribute('name'),
-  //       ...formState,
-  //     }),
-  //   })
-  //     .then(console.log(encode({ 'form-name': 'contact', ...formState })))
-  //     // eslint-disable-next-line no-undef
-  //     .catch((error) => alert(error));
-  // };
-
   return (
     <StyledForm
       name="contact v1"
@@ -95,38 +52,16 @@ export default function ContactForm() {
       <input type="hidden" name="form-name" value="contact v1" />
       <p hidden>
         <label>
-          Dont fill this out:
           <input name="bot-field" />
         </label>
       </p>
 
       <label htmlFor="name">Name</label>
-      <input
-        // id="name"
-        type="text"
-        name="name"
-        // onChange={handleChange}
-        // value={formState.name}
-        placeholder="Enter your name"
-      />
+      <input type="text" name="name" placeholder="Enter your name" />
       <label htmlFor="email">Email</label>
-      <input
-        // id="email"
-        type="email"
-        name="email"
-        // onChange={handleChange}
-        // value={formState.email}
-        placeholder="Enter your email"
-      />
+      <input type="email" name="email" placeholder="Enter your email" />
       <label htmlFor="message">Message</label>
-      <textarea
-        // id="message"
-        type="text"
-        name="message"
-        // onChange={handleChange}
-        // value={formState.message}
-        placeholder="Enter your message"
-      />
+      <textarea type="text" name="message" placeholder="Enter your message" />
       <button type="submit">Submit</button>
     </StyledForm>
   );
