10a75d340fa0741eeb2b7fc330eaab554b4da7af
starczi <jak.starczewski@gmail.com>
2021-02-10 22:55:12 +0100

    Add page for functionals

--- src/pages/functional.js
+++ src/pages/functional.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql, Link, useStaticQuery } from 'gatsby';
 import styled from 'styled-components';
-import Img from 'gatsby-image';
+import BackgroundImage from 'gatsby-background-image';
 import Layout from '../components/Layout/layout';
 
 export default function Functional() {
@@ -9,20 +9,92 @@ export default function Functional() {
     query {
       file(relativePath: { eq: "mak1.jpg" }) {
         childImageSharp {
-          fixed(width: 268, height: 268, quality: 100) {
+          fixed(width: 260, height: 260, quality: 100) {
             ...GatsbyImageSharpFixed_noBase64
+          }
+        }
+      }
+      file(relativePath: { eq: "mak1.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 260, maxHeight: 260, quality: 100) {
+            ...GatsbyImageSharpFluid_noBase64
           }
         }
       }
     }
   `);
 
+  const StyledBackgroundImage = styled(BackgroundImage)`
+    box-shadow: 0 0 40px rgb(0 0 0 / 10%);
+
+    margin: 91.5px 1rem 0 1rem;
+
+    @media (max-width: 1167px) {
+      margin-top: 2rem;
+    }
+  `;
+
+  const BlackOverlay = styled.div`
+    display: flex;
+    height: 100%;
+    align-items: center;
+    transition: background-color 0.3s cubic-bezier(0.39, 0.575, 0.565, 1);
+
+    :hover {
+      background-color: rgba(0, 0, 0, 0.3);
+    }
+
+    div {
+      position: relative;
+      padding: 1rem;
+      max-width: 8rem;
+      margin: 0 auto;
+      background-color: white;
+      text-transform: uppercase;
+
+      &::before {
+        top: -5px;
+        left: -5px;
+        right: -5px;
+        bottom: -5px;
+        content: '';
+        position: absolute;
+        border: 1px solid #fff;
+      }
+    }
+  `;
+
   return (
     <Layout>
       <h1> Makramy użytkowe</h1>
-      <div>
-        <Img fixed={data.file.childImageSharp.fixed} />
-      </div>
+      <StyledBackgroundImage fixed={data.file.childImageSharp.fixed}>
+        <Link to="/">
+          <BlackOverlay>
+            <div>torebki</div>
+          </BlackOverlay>
+        </Link>
+      </StyledBackgroundImage>
+      <StyledBackgroundImage fixed={data.file.childImageSharp.fixed}>
+        <Link to="/">
+          <BlackOverlay>
+            <div>kwietniki</div>
+          </BlackOverlay>
+        </Link>
+      </StyledBackgroundImage>
+      <StyledBackgroundImage fixed={data.file.childImageSharp.fixed}>
+        <Link to="/">
+          <BlackOverlay>
+            <div>bieżniki</div>
+          </BlackOverlay>
+        </Link>
+      </StyledBackgroundImage>
+      <StyledBackgroundImage fixed={data.file.childImageSharp.fixed}>
+        <Link to="/">
+          <BlackOverlay>
+            <div>półki</div>
+          </BlackOverlay>
+        </Link>
+      </StyledBackgroundImage>
     </Layout>
   );
 }
