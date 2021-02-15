d1293ee65ba51d7e490f8934e49a681ece9d3096
Jakub Starczewski <starczi@users.noreply.github.com>
2021-02-15 20:58:00 +0100

    update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,16 +29,16 @@ module.exports = {
         name: 'images',
       },
     },
-    {
-      resolve: 'gatsby-source-cloudinary',
-      options: {
-        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-        apiKey: process.env.CLOUDINARY_API_KEY,
-        apiSecret: process.env.CLOUDINARY_API_SECRET,
-        resourceType: 'image',
-        prefix: 'gatsby-source/',
-      },
-    },
+    // {
+    //   resolve: 'gatsby-source-cloudinary',
+    //   options: {
+    //     cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    //     apiKey: process.env.CLOUDINARY_API_KEY,
+    //     apiSecret: process.env.CLOUDINARY_API_SECRET,
+    //     resourceType: 'image',
+    //     prefix: 'gatsby-source/',
+    //   },
+    // },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,7 @@
-import CMS from 'netlify-cms-app';
+// import CMS from 'netlify-cms-app';
 
-// You only need to import the media library that you'll use. We register both
-// here for example purposes.
-import cloudinary from 'netlify-cms-media-library-cloudinary';
+// // You only need to import the media library that you'll use. We register both
+// // here for example purposes.
+// import cloudinary from 'netlify-cms-media-library-cloudinary';
 
-CMS.registerMediaLibrary(cloudinary);
+// CMS.registerMediaLibrary(cloudinary);
